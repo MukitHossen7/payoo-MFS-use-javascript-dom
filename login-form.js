7f65de0564a76fecb9d180f8f144c29474a61225
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-18 23:29:27 +0600

    login form is done

--- login-form.js
+++ login-form.js
@@ -1,3 +1,14 @@
 document.getElementById("login-btn").addEventListener("click", function (e) {
   e.preventDefault();
+  let numInput = document.getElementById("num-input");
+  let numValue = numInput.value;
+  let pinInput = document.getElementById("pin-input");
+  let pinValue = pinInput.value;
+  if (numValue === "01326153447" && pinValue === "1234") {
+    window.location.href = "./home-page.html";
+  } else {
+    alert("Wrong phone number or pin number");
+  }
+  numInput.value = "";
+  pinInput.value = "";
 });
