7a0877c10832d4aced386597ec3f2e6d2082f09d
MukitHossen7 <hossenmukit7@gmail.com>
2024-09-19 08:23:43 +0600

    add event handler and click the button add your money

---
+++ add-money.js
@@ -0,0 +1,15 @@
+document.getElementById("addBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const inputAmount = document.getElementById("inputAmount");
+  const amountValue = inputAmount.value;
+  const inputPin = document.getElementById("inputPin");
+  const pinValue = inputPin.value;
+  let money = document.getElementById("money");
+  if (pinValue === "1234") {
+    let totalMoney = money.innerText;
+    money.innerText = Number(totalMoney) + Number(amountValue);
+  }
+  console.log(amountValue, pinValue);
+  inputAmount.value = "";
+  inputPin.value = "";
+});
